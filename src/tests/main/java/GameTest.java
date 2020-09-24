41cf023936f103c840a51a8cc785dcb95ff3bc82
Calvin Owens <calvinmowens@gmail.com>
2020-09-24 11:04:07 -0400

    setup tests folder in IntelliJ and created a test for Game.java

---
+++ src/tests/main/java/GameTest.java
@@ -0,0 +1,15 @@
+package main.java;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameTest {
+
+    private Game game;
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+        game = new Game();
+    }
+}
