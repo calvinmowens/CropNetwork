14462b75d40e2c8656dd7a1fb2b667cae0caa5a2
Calvin Owens <calvinmowens@gmail.com>
2020-09-24 13:41:45 -0400

    seed modal is workinggit add .

--- src/main/controllers/game_configController.java
+++ src/main/controllers/game_configController.java
@@ -2,6 +2,7 @@ package main.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -13,14 +14,36 @@ import javafx.stage.Stage;
 import main.java.Game;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class game_configController {
+public class game_configController implements Initializable {
 
     public TextField name;
     public ToggleGroup gender;
-    public ChoiceBox difficulty;
-    public ChoiceBox startingSeed;
-    public ChoiceBox startingSeason;
+    public ChoiceBox<String> difficulty;
+    public ChoiceBox<String> startingSeed;
+    public ChoiceBox<String> startingSeason;
+
+    /**
+     * This method initializes out choice boxes with the selection items we want
+     * them to have.
+     * @param url
+     * @param resourceBundle
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        difficulty.getItems().add("Casual");
+        difficulty.getItems().add("Regular");
+        difficulty.getItems().add("Hardcore");
+
+        startingSeed.getItems().add("");
+
+        startingSeason.getItems().add("Spring");
+        startingSeason.getItems().add("Summer");
+        startingSeason.getItems().add("Fall");
+        startingSeason.getItems().add("Winter");
+    }
 
     public void startGame(ActionEvent actionEvent) throws IOException {
 
@@ -28,16 +51,15 @@ public class game_configController {
         Game newGame = new Game();
         newGame.setName(name.getText());
         newGame.setGender(((RadioButton)gender.getSelectedToggle()).getText());
-        newGame.setDifficulty();
 
         // setting up the loader
         FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(getClass().getResource("main_ui.FXML"));
+        loader.setLocation(getClass().getResource("../screens/main_ui.FXML"));
         Parent startGame = loader.load();
         Scene startGameScene = new Scene(startGame);
 
-        main_uiController controller = loader.getController();
-        controller.initData(newGame);
+//        main_uiController controller = loader.getController();
+//        controller.initData(newGame);
 
         // Stage and show the new scene
         Stage window = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
@@ -45,5 +67,4 @@ public class game_configController {
         window.setScene(startGameScene);
         window.show();
     }
-
 }
--- src/main/controllers/main_uiController.java
+++ src/main/controllers/main_uiController.java
@@ -1,6 +1,11 @@
 package main.controllers;
 
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import main.java.Game;
 
 import java.net.URL;
@@ -9,6 +14,11 @@ import java.util.ResourceBundle;
 public class main_uiController implements Initializable {
 
     private static Game myGame;
+    public AnchorPane background;
+    public ImageView background_season;
+    public AnchorPane seed_modal;
+
+    private BooleanProperty seedBagClick = new SimpleBooleanProperty(false);
 
     /**
      * TODO:
@@ -23,20 +33,23 @@ public class main_uiController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        seed_modal.visibleProperty().bind(seedBagClick);
+        background.visibleProperty().bind(seedBagClick);
     }
 
     /**
      * This method initializes the Game UI with settings from the prev scene
+     * TODO: Parth implement this
      * @param game  param to get information from prev scene
      */
     public void initData(Game game) {
         myGame = game;
-        // name
-        // season
-        // money - based on difficulty
-        // gender
+        String[] seasonImages = {"spring.png", "summer.png", "fall.png", "winter.png"};
+
     }
 
 
+    public void toggleSeedModal(ActionEvent actionEvent) {
+        seedBagClick.set(!seedBagClick.get());
+    }
 }
