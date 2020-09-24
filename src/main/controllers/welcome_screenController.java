98d4ca5c6506ee0bbf226e8db2024530f3c0192a
Calvin Owens <calvinmowens@gmail.com>
2020-09-24 10:54:40 -0400

    change .gitignore

--- src/main/controllers/welcome_screenController.java
+++ src/main/controllers/welcome_screenController.java
@@ -12,9 +12,12 @@ import java.io.IOException;
 public class welcome_screenController {
 
     public void changeScreen(MouseEvent mouseEvent) throws IOException {
+
         Parent root = FXMLLoader.load(getClass().getResource("../screens/game_config.FXML"));
         Scene scene = new Scene(root);
+
         Stage window = (Stage)((Node) mouseEvent.getSource()).getScene().getWindow();
+
         window.setScene(scene);
         window.show();
 
