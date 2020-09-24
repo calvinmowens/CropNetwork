2003bf1b20e05b5be81ca228b36aba4224f50d19
Calvin Owens <calvinmowens@gmail.com>
2020-09-24 10:50:52 -0400

    actually link controllers this time, and change some FXML properties on game_config (select group and text align)

--- src/main/controllers/game_configController.java
+++ src/main/controllers/game_configController.java
@@ -1,4 +1,31 @@
 package main.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class game_configController {
+
+    public void startGame(ActionEvent actionEvent) throws IOException {
+
+        /**
+         * TODO:
+         * Scrape this page of contents and save to Game.java before continuing.
+         * Pass to main_ui controller.
+         */
+
+        Parent startGame = FXMLLoader.load(getClass().getResource("../screens/main_ui.FXML"));
+        Scene startGameScene = new Scene(startGame);
+
+        Stage window = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+
+        window.setScene(startGameScene);
+        window.show();
+    }
+
 }
--- src/main/controllers/main_uiController.java
+++ src/main/controllers/main_uiController.java
@@ -1,4 +1,10 @@
 package main.controllers;
 
 public class main_uiController {
+
+    /**
+     * TODO:
+     * Create clock method and update time on 10ms basis. Best way to do this?
+     * Update elements based on information from Game.java
+     */
 }
