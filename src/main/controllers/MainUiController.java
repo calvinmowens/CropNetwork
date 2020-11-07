926cddd90ea44867ae6a562bf7567151a18ae88e
Calvin Owens <calvinmowens@gmail.com>
2020-11-07 18:21:27 -0500

    unworking, but progressing

--- src/main/controllers/MainUiController.java
+++ src/main/controllers/MainUiController.java
@@ -518,17 +518,8 @@ public class MainUiController implements Initializable {
         CropPlot[] myPlots = myGame.getPlots();
         int plotId = Integer.parseInt(id.substring(4)) - 1;
         CropPlot myCrop = myPlots[plotId];
-<<<<<<< HEAD
-        if (myCrop.getMaturity() != 4 || myCrop.getMaturity() != 0) { // if crop is not dead, water
-            myCrop.setWaterLevel(myCrop.getWaterLevel() + 1);
-            if (myCrop.getWaterLevel() == 4) { // if water goes beyond full, kill it
-                killCrop(myCrop);
-            }
-        }
-=======
 
         myCrop.waterCrop();
->>>>>>> calvin
         this.initData(myGame);
     }
 
